5d86f1c5d7f47350692982016d4e58725164155b
Gen Gulay <gengulay@gmail.com>
2018-09-19 09:11:45 +0800

    Torjackan url bypasser

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('torjackanController', ['$scope', function($scope) {
+  $scope.greeting = 'Hola!';
+
+
+$scope.torjack = function () {
+
+if($scope.name == undefined) {
+  $scope.name = "";
+}
+
+
+    var n = $scope.name.indexOf("patorjack");
+
+    var newString = $scope.name.substr(n)
+
+    newString = newString.replace(/%2F/g, "/");
+
+    $scope.result = newString;
+
+    $scope.name="";
+
+
+
+
+}
+
+
+
+
+
+
+}]);
